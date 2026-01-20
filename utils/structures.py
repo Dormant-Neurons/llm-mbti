36b5ca9dd2ecc595b3edcb05ea108c22609b9506
LostOxygen <jonathan.evertz@gmail.com>
2026-01-20 16:48:00 +0100

    fixed Answer structure

--- utils/structures.py
+++ utils/structures.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 class Answer(BaseModel):
     """Represents an structured answer for the model output"""
 
-    answer: str
+    answer: int
     explanation: str
 
 class HexacoAnswer(BaseModel):
