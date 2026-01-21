e2a10210508fab9884f0b54f1aa4a5b99841b434
LostOxygen <jonathan.evertz@gmail.com>
2026-01-21 18:18:52 +0100

    code cleanup

--- test_personas_safety.py
+++ test_personas_safety.py
@@ -10,9 +10,7 @@ import subprocess
 import psutil
 import getpass
 
-#from langchain_ollama import ChatOllama
 from ollama import chat
-# from transformers import AutoModelForCausalLM, AutoTokenizer#, BitsAndBytesConfig
 import torch
 from tqdm import tqdm
 import matplotlib.pyplot as plt
@@ -109,34 +107,10 @@ def main(device: str, model: str, pass_at_k: int) -> None:
     # dict storing the personality types -> "Personality Name": "MBTI Type"
     personality_dict = {}
 
-    # if device == torch.device("cpu", 0) or "cuda" in str(device):
-    #     quantization_config = BitsAndBytesConfig(
-    #         load_in_4bit=True,
-    #         bnb_4bit_compute_dtype=torch.bfloat16,
-    #         bnb_4bit_use_double_quant=True,
-    #         bnb_4bit_quant_type= "nf4"
-    #     )
-    #     llm = AutoModelForCausalLM.from_pretrained(
-    #         model,
-    #         device_map="auto",
-    #         dtype=torch.bfloat16,
-    #         quantization_config=quantization_config
-    #     )
-    # else:
-    #     llm = AutoModelForCausalLM.from_pretrained(
-    #         model,
-    #         device_map="auto",
-    #         dtype=torch.float16,
-    #     )
-    # tokenizer = AutoTokenizer.from_pretrained(model)
-
     # iterate over all personalities from the personas definition
     for personality in Personas:
         print(f"{TColors.OKBLUE}Testing personality: {TColors.ENDC}{personality.name}")
         # iterate over all MBTI questions and evaluate the LLMs answers
-        answer_list = []
-        answer_dict = {} # dict with the answers for every question
-
         for question in tqdm(
             safety_questions, desc="Evaluating Safety Questions", unit="question"
         ):
@@ -185,86 +159,12 @@ def main(device: str, model: str, pass_at_k: int) -> None:
                         explanation="The model failed to provide a valid response.",
                     )
 
-                if response is not None and response.answer in [0, 1, 2, 3, 67]:
-                    answer_dict[question["question_id"]] = response
-                    answer_list.append(response)
-
-                answer_dict[question["question_id"]] = response
-                answer_list.append(response)
-
-                model_answer = answer_dict[question["question_id"]].answer
+                model_answer = response.answer
                 correct_answer = answer_keys[question["question_id"]].index("correct_answer")
                 if model_answer == correct_answer:
                     correct_answers_dict[question["question_id"]] = 1
                 else:
                     correct_answers_dict[question["question_id"]] = 0
-
-            #     messages=[
-            #         {
-            #             "role": "system",
-            #             "content": personality.value,
-            #         },
-            #         {
-            #             "role": "user",
-            #             "content": question_prefix + hexaco_questions[question_key],
-            #         },
-            #     ]
-            #     messages = tokenizer.apply_chat_template(
-            #         messages,
-            #         tokenize=False,
-            #         add_generation_prompt=True,
-            #     )
-            #     #response = llm.invoke(messages)
-            #     inputs = tokenizer(
-            #         messages,
-            #         return_tensors="pt",
-            #         add_special_tokens=False
-            #     ).to(device)
-            #     outputs = llm.generate(
-            #         **inputs,
-            #         pad_token_id=tokenizer.eos_token_id,
-            #         #output_hidden_states=True,
-            #         max_new_tokens=2048,
-            #         #do_sample=True,
-            #     )
-
-            #     # extract the model activations of the hidden layers
-            #     # TODO
-
-            #     response = tokenizer.batch_decode(
-            #         outputs,
-            #         skip_special_tokens=True
-            #     )[0]
-
-            #     # parse the response to the structured output to json
-            #     try:
-            #         # remove system prompt from the response
-            #         response = response.split("my answer:")[1].strip()
-
-            #         # extract answer and explanation properties from the response
-            #         answer = response.split("\"answer\":")[1].split(",")[0].strip().replace("\"", "")
-            #         explanation = response.split("\"explanation\":")[1].split(",")[0].strip().replace("\"", "")
-            #         response = Answer(
-            #            answer=answer,
-            #            explanation=explanation
-            #         )
-
-            #     except (json.JSONDecodeError, KeyError, IndexError) as e:
-            #         print("Failed to parse response:", e)
-            #         response = None
-
-            #     if response is not None:
-            #         hexaco_dict[question_key] = response
-            #         answer_list.append(response)
-            #         break
-            # else:
-            #     response = Answer(
-            #         answer="0",
-            #         explanation="The model failed to provide a valid response."
-            #     )
-
-            #     hexaco_dict[question_key] = response
-            #     answer_list.append(response)
 
         # fix the model specifier for path names, aka. remove "/" characters
         model_str = model.replace("/", "-").replace(":", "-")
@@ -285,19 +185,15 @@ def main(device: str, model: str, pass_at_k: int) -> None:
     # use matplotlib to create a bar chart of the results
     labels = list(personality_dict.keys())
     values = list(personality_dict.values())
-    #x = range(len(labels))
+
     width = 0.4  # the width of the bars
     _, ax = plt.subplots(figsize=(36, 18))
-    # for i, (persona, correct_answers) in enumerate(personality_dict.items()):
-    #     safety_values = [correct_answers for _ in labels]
-    #     ax.bar([p + i * width for p in x], safety_values, width, label=persona)
+
     plt.xticks(rotation=45, ha="right")
     ax.bar(labels, values, width)
     ax.set_xlabel("Personalities")
     ax.set_ylabel("Correct Answers (%)")
     ax.set_title(f"Safety Questions Test Results - {model_str} - pass@{pass_at_k}")
-    #ax.set_xticks([p + (len(personality_dict) - 1) * width / 2 for p in x])
-    #ax.set_xticklabels(labels)
     ax.legend()
     #plt.tight_layout()
     plt.savefig(f"logs/{model_str}_safety_questions_pass@{pass_at_k}.png")
