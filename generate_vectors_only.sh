8a00a8e6f46efeb5fd70b93793d2e1a55845088d
LostOxygen <jonathan.evertz@gmail.com>
2026-05-04 14:05:10 +0200

    removed thresholds

--- generate_vectors_only.sh
+++ generate_vectors_only.sh
@@ -10,7 +10,6 @@ python generate_vec.py \
     --neg_path eval_persona_extract/mlabonne-gemma-3-27b-it-abliterated/apathetic_neg_instruct.csv \
     --trait apathetic \
     --save_dir persona_vectors/mlabonne-gemma-3-27b-it-abliterated/apathetic \
-    --threshold 0
 
 # EVIL
 python generate_vec.py \
@@ -19,7 +18,6 @@ python generate_vec.py \
     --neg_path eval_persona_extract/mlabonne-gemma-3-27b-it-abliterated/evil_neg_instruct.csv \
     --trait evil \
     --save_dir persona_vectors/mlabonne-gemma-3-27b-it-abliterated/evil \
-    --threshold 0
 
 # FEARFUL
 python generate_vec.py \
@@ -28,7 +26,6 @@ python generate_vec.py \
     --neg_path eval_persona_extract/mlabonne-gemma-3-27b-it-abliterated/fearful_neg_instruct.csv \
     --trait fearful \
     --save_dir persona_vectors/mlabonne-gemma-3-27b-it-abliterated/fearful \
-    --threshold 0
 
 # HALLUCINATING
 python generate_vec.py \
@@ -37,7 +34,6 @@ python generate_vec.py \
     --neg_path eval_persona_extract/mlabonne-gemma-3-27b-it-abliterated/hallucinating_neg_instruct.csv \
     --trait hallucinating \
     --save_dir persona_vectors/mlabonne-gemma-3-27b-it-abliterated/hallucinating \
-    --threshold 0
 
 # HONESTY
 python generate_vec.py \
@@ -46,7 +42,6 @@ python generate_vec.py \
     --neg_path eval_persona_extract/mlabonne-gemma-3-27b-it-abliterated/honesty_neg_instruct.csv \
     --trait honesty \
     --save_dir persona_vectors/mlabonne-gemma-3-27b-it-abliterated/honesty \
-    --threshold 0
 
 # HUMOROUS
 python generate_vec.py \
@@ -55,7 +50,6 @@ python generate_vec.py \
     --neg_path eval_persona_extract/mlabonne-gemma-3-27b-it-abliterated/humorous_neg_instruct.csv \
     --trait humorous \
     --save_dir persona_vectors/mlabonne-gemma-3-27b-it-abliterated/humorous \
-    --threshold 0
 
 # IMPOLITE
 python generate_vec.py \
@@ -64,7 +58,6 @@ python generate_vec.py \
     --neg_path eval_persona_extract/mlabonne-gemma-3-27b-it-abliterated/impolite_neg_instruct.csv \
     --trait impolite \
     --save_dir persona_vectors/mlabonne-gemma-3-27b-it-abliterated/impolite \
-    --threshold 0
 
 # MALEVOLENT
 python generate_vec.py \
@@ -73,7 +66,6 @@ python generate_vec.py \
     --neg_path eval_persona_extract/mlabonne-gemma-3-27b-it-abliterated/malevolent_neg_instruct.csv \
     --trait malevolent \
     --save_dir persona_vectors/mlabonne-gemma-3-27b-it-abliterated/malevolent \
-    --threshold 0
 
 # OPTIMISTIC
 python generate_vec.py \
@@ -82,7 +74,6 @@ python generate_vec.py \
     --neg_path eval_persona_extract/mlabonne-gemma-3-27b-it-abliterated/optimistic_neg_instruct.csv \
     --trait optimistic \
     --save_dir persona_vectors/mlabonne-gemma-3-27b-it-abliterated/optimistic \
-    --threshold 0
 
 # SYCOPHANTIC
 python generate_vec.py \
@@ -91,7 +82,6 @@ python generate_vec.py \
     --neg_path eval_persona_extract/mlabonne-gemma-3-27b-it-abliterated/sycophantic_neg_instruct.csv \
     --trait sycophantic \
     --save_dir persona_vectors/mlabonne-gemma-3-27b-it-abliterated/sycophantic \
-    --threshold 0
 
 
 # SOCIALLY ADEPT
@@ -101,4 +91,3 @@ python generate_vec.py \
     --neg_path eval_persona_extract/mlabonne-gemma-3-27b-it-abliterated/socially_adept_neg_instruct.csv \
     --trait socially_adept \
     --save_dir persona_vectors/mlabonne-gemma-3-27b-it-abliterated/socially_adept \
-    --threshold 0
